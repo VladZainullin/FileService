fb9ab0e9e6a4c51e9896ae85ab18a08ed47c564d
Vladislav Zainullin <mittoface02@mail.ru>
2023-09-05 09:53:55 +0300

    Создаёт миграции

---
+++ Infrastructure/Persistence/Migrations/20230905065248_init.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Resources",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    TypeId = table.Column<int>(type: "integer", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    ParentId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Resources", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Resources_Resources_ParentId",
+                        column: x => x.ParentId,
+                        principalTable: "Resources",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Resources",
+                columns: new[] { "Id", "ParentId", "Title", "TypeId" },
+                values: new object[] { new Guid("db4eedce-9959-4800-8d41-65e348611667"), new Guid("db4eedce-9959-4800-8d41-65e348611667"), "/Users/vadislavzainullin/Downloads/Root", 1 });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Resources_ParentId",
+                table: "Resources",
+                column: "ParentId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Resources");
+        }
+    }
+}
